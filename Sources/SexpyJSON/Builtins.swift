f35e15909372186535cafa8ab09e16e66e55eb59
Juri Pakaste <juri@juripakaste.fi>
2021-09-06 07:49:09 +0300

    Add subtract function

--- Sources/SexpyJSON/Builtins.swift
+++ Sources/SexpyJSON/Builtins.swift
@@ -1,3 +1,4 @@
 let builtins: [Symbol: IntermediateValue] = [
     .addition: .function(.addFunction),
+    .subtraction: .function(.subtractFunction),
 ]
--- Tests/SexpyJSONTests/EvaluatorMathTests.swift
+++ Tests/SexpyJSONTests/EvaluatorMathTests.swift
@@ -46,4 +46,49 @@ final class EvaluatorMathTests: XCTestCase {
         let outputValue = try evaluateToOutput(expression: expr, in: .withBuiltins)
         XCTAssertEqual(try XCTUnwrap(outputValue.number), 20.0, accuracy: 0.0001)
     }
+
+    // MARK: Subtract
+
+    func testSubtractOneNumber() throws {
+        let expr = Expression.call(.init(target: .symbol(.subtraction), params: [.value(.number("-68"))]))
+        let outputValue = try evaluateToOutput(expression: expr, in: .withBuiltins)
+        XCTAssertEqual(try XCTUnwrap(outputValue.number), -68.0, accuracy: 0.0001)
+    }
+
+    func testSubtractTwoNumbers() throws {
+        let expr = Expression.call(
+            .init(target: .symbol(.subtraction), params: [.value(.number("1")), .value(.number("2"))])
+        )
+        let outputValue = try evaluateToOutput(expression: expr, in: .withBuiltins)
+        XCTAssertEqual(try XCTUnwrap(outputValue.number), -1.0, accuracy: 0.0001)
+    }
+
+    func testSubtractThreeNumbers() throws {
+        let expr = Expression.call(
+            .init(
+                target: .symbol(.subtraction),
+                params: [.value(.number("1")), .value(.number("2")), .value(.number("-4"))]
+            )
+        )
+        let outputValue = try evaluateToOutput(expression: expr, in: .withBuiltins)
+        XCTAssertEqual(try XCTUnwrap(outputValue.number), 3.0, accuracy: 0.0001)
+    }
+
+    func testSubtract() throws {
+        let expr = Expression.call(
+            .init(
+                target: .symbol(.subtraction),
+                params: [
+                    .value(.number("1")),
+                    .call(.init(target: .symbol(.subtraction), params: [
+                        .value(.number("11")),
+                        .value(.number("12")),
+                    ])),
+                    .value(.number("-4")),
+                ]
+            )
+        )
+        let outputValue = try evaluateToOutput(expression: expr, in: .withBuiltins)
+        XCTAssertEqual(try XCTUnwrap(outputValue.number), 6.0, accuracy: 0.0001)
+    }
 }
