0e6a36a38b714c9ccc95b91466d34d0a89802fb8
Juri Pakaste <juri@juripakaste.fi>
2021-09-06 07:39:46 +0300

    Add an evaluator for Expressions

---
+++ Sources/SexpyJSON/Evaluator.swift
@@ -0,0 +1,107 @@
+struct Context {
+    var namespace: Namespace
+
+    func value(for symbol: Symbol) throws -> IntermediateValue {
+        return try namespace[symbol]
+    }
+}
+
+final class Namespace {
+    let names: [Symbol: IntermediateValue]
+    let wrappedNamespace: Namespace?
+
+    init(
+        names: [Symbol: IntermediateValue],
+        wrappedNamespace: Namespace?
+    ) {
+        self.names = names
+        self.wrappedNamespace = wrappedNamespace
+    }
+
+    subscript(key: Symbol) -> IntermediateValue {
+        get throws {
+            if let value = self.names[key] {
+                return value
+            }
+            if let wrappedNamespace = wrappedNamespace {
+                return try wrappedNamespace[key]
+            }
+            throw EvaluatorError.missingValue(key)
+        }
+    }
+}
+
+struct Function {
+    let f: ([Expression], inout Context) throws -> IntermediateValue
+
+    func call(_ params: [Expression], context: inout Context) throws -> IntermediateValue {
+        try self.f(params, &context)
+    }
+}
+
+enum IntermediateValue {
+    case function(Function)
+    case string(String)
+    case number(Double)
+    case array([IntermediateValue])
+    case object([IntermediateObjectMember])
+    case boolean(Bool)
+    case null
+}
+
+struct IntermediateObjectMember {
+    var name: String
+    var value: IntermediateValue
+}
+
+func evaluate(expression: Expression, in context: inout Context) throws -> IntermediateValue {
+    switch expression {
+    case let .value(val):
+        return try evaluateValue(value: val, in: &context)
+    case let .symbol(symbol):
+        return try evaluateSymbol(symbol: symbol, in: &context)
+    case let .call(call):
+        return try evaluateCall(call: call, in: &context)
+    }
+}
+
+func evaluateValue(value: ExpressionValue, in context: inout Context) throws -> IntermediateValue {
+    switch value {
+    case let .string(string):
+        return .string(string)
+    case let .number(string):
+        guard let num = numberFormatter.number(from: string)?.doubleValue else {
+            throw EvaluatorError.badExpressionType(value)
+        }
+        return .number(num)
+    case let .array(array):
+        return try .array(array.map { try evaluate(expression: $0, in: &context) })
+    case let .object(array):
+        return try .object(array.map { IntermediateObjectMember(name: $0.name, value: try evaluate(expression: $0.value, in: &context)) })
+    case let .boolean(bool):
+        return .boolean(bool)
+    case .null:
+        return .null
+    case let .expression(expression):
+        return try evaluate(expression: expression, in: &context)
+    }
+}
+
+func evaluateSymbol(symbol: Symbol, in context: inout Context) throws -> IntermediateValue {
+    try context.value(for: symbol)
+}
+
+func evaluateCall(call: Call, in context: inout Context) throws -> IntermediateValue {
+    let target = try evaluate(expression: call.target, in: &context)
+    guard case let .function(fun) = target else {
+        throw EvaluatorError.badCallTarget(target)
+    }
+    return try fun.call(call.params, context: &context)
+}
+
+enum EvaluatorError: Error {
+    case badCallTarget(IntermediateValue)
+    case badExpressionType(ExpressionValue)
+    case badParameterList([Expression], String)
+    case missingValue(Symbol)
+}
