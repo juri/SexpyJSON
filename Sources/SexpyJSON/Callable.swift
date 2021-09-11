7b3810711fa6b8f84cf39ac73e9dd784c6b90ea5
Juri Pakaste <juri@juripakaste.fi>
2021-09-11 13:01:41 +0300

    Add more function types

--- Sources/SexpyJSON/Callable.swift
+++ Sources/SexpyJSON/Callable.swift
@@ -1,13 +1,25 @@
 enum Callable {
     case function1(Function1)
+    case function2WithContext(FunctionWithContext2)
     case functionVarargs(FunctionVarargs)
+    case functionVarargsWithContext(FunctionVarargsWithContext)
     case specialOperator(SpecialOperator)
+
     func call(_ params: [Expression], context: inout Context) throws -> IntermediateValue {
         switch self {
         case let .specialOperator(fun):
             return try fun.call(params, context: &context)
         case let .function1(fun):
             return try fun.call(params, context: &context)
+        case let .function2WithContext(fun):
+            return try fun.call(params, context: &context)
+        case let .functionVarargs(fun):
+            return try fun.call(params, context: &context)
+        case let .functionVarargsWithContext(fun):
+            return try fun.call(params, context: &context)
+        }
+    }
+
         case let .functionVarargs(fun):
             return try fun.call(params, context: &context)
         }
@@ -34,6 +46,19 @@ struct Function1 {
 
         return try self.f(paramValue)
     }
+struct FunctionWithContext2 {
+    let f: (IntermediateValue, IntermediateValue, inout Context) throws -> IntermediateValue
+    let name: String
+
+    func call(_ params: [Expression], context: inout Context) throws -> IntermediateValue {
+        guard params.count == 2 else {
+            throw EvaluatorError.badParameterList(params, "\(self.name) requires two arguments")
+        }
+        let param1Value = try evaluate(expression: params[0], in: &context)
+        let param2Value = try evaluate(expression: params[1], in: &context)
+
+        return try self.f(param1Value, param2Value, &context)
+    }
 }
 
 struct FunctionVarargs {
@@ -42,5 +67,13 @@ struct FunctionVarargs {
     func call(_ params: [Expression], context: inout Context) throws -> IntermediateValue {
         let paramValues = try params.map { try evaluate(expression: $0, in: &context) }
         return try self.f(paramValues)
+    }
+
+struct FunctionVarargsWithContext {
+    let f: ([IntermediateValue], Context) throws -> IntermediateValue
+
+    func call(_ params: [Expression], context: inout Context) throws -> IntermediateValue {
+        let paramValues = try params.map { try evaluate(expression: $0, in: &context) }
+        return try self.f(paramValues, context)
     }
 }
