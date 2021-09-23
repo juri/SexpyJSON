0ff60cf33d087b272b2ee74b2c4c42c471de663f
Juri Pakaste <juri@juripakaste.fi>
2021-09-23 21:29:00 +0300

    Basic AsciiDoc output for the doc extractor

--- Sources/FunctionDocExtractor/ExtractFunctionDocs.swift
+++ Sources/FunctionDocExtractor/ExtractFunctionDocs.swift
@@ -6,6 +6,29 @@ import SwiftSyntax
 enum Extractor {
     static func main() throws {
         let docResults = try extractFunctionDocumentation()
-        dump(docResults)
+        let docs = try docResults
+            .map { _, result in try result.get() }
+            .sorted { $0.name < $1.name }
+        for doc in docs {
+            print("=== \(doc.name)")
+            for part in doc.parts {
+                print()
+                switch part {
+                case let .example(ex):
+                    print("----")
+                    print(ex.content)
+                    print("----")
+                    if let expect = ex.expectedReturn {
+                        print()
+                        print("----")
+                        print("==> \(expect)")
+                        print("----")
+                    }
+                case let .text(text):
+                    print(text.content)
+                }
+            }
+            print()
+        }
     }
 }
