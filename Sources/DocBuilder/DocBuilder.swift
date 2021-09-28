b8ee9717efdc7b044e253367b9f70069be36c880
Juri Pakaste <juri@juripakaste.fi>
2021-09-28 21:39:00 +0300

    Add documentation updater subcommand to DocBuilder

--- Sources/DocBuilder/DocBuilder.swift
+++ Sources/DocBuilder/DocBuilder.swift
@@ -3,11 +3,11 @@ import Foundation
 import FunctionDocExtractorCore
 
 @main
-@available(macOS 10.13, *)
+@available(macOS 11.0, *)
 struct DocBuilder: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "Documentation builder for SexpyJSON.",
-        subcommands: [Extract.self, BuildRaw.self, Build.self]
+        subcommands: [Extract.self, BuildRaw.self, Build.self, UpdateGitHubPages.self]
     )
 }
 
@@ -49,6 +49,93 @@ struct Build: ParsableCommand {
     func run() throws {
         let files = try createUnprocessed(target: self.target.name)
         try processAsciiDoc(files: files, to: self.target.name, with: self.processor)
+    }
+}
+
+@available(macOS 11.0, *)
+struct UpdateGitHubPages: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        abstract: "Build processed language documentation and update GitHub Pages."
+    )
+
+    @Option(help: "AsciiDoctor executable to use. Default: asciidoctor from path", transform: URL.init(fileURLWithPath:))
+    var processor: URL?
+
+    func run() throws {
+        guard try runCapturingFromPath("git", args: ["status", "--porcelain"]).isEmpty else {
+            throw errorExit("The working directory is not clean")
+        }
+
+        guard let branchName = try runCapturingFromPath("git", args: ["rev-parse", "--abbrev-ref", "HEAD"]).utf8String,
+              !branchName.isEmpty
+        else { throw errorExit("Failed to read current branch") }
+
+        guard branchName == "main" else { throw errorExit("Not on main branch") }
+
+        guard let repoRoot = try runCapturingFromPath("git", args: ["rev-parse", "--show-toplevel"]).utf8String,
+              !repoRoot.isEmpty
+        else {
+            throw errorExit("Failed to read repository root")
+        }
+
+        print("🍒 Changing to repository root folder \(repoRoot)")
+
+        guard FileManager.default.changeCurrentDirectoryPath(repoRoot) else {
+            throw errorExit("Failed to change working directory to \(repoRoot)")
+        }
+
+        print("🍒 Switching to gh-pages branch")
+
+        try runNoisyFromPath("git", args: ["switch", "--no-guess", "gh-pages"])
+
+        print("🍒 Rebasing gh-pages on main")
+
+        try runNoisyFromPath("git", args: ["rebase", "main"])
+        let temp = try FileManager.default.url(
+            for: .itemReplacementDirectory,
+            in: .userDomainMask,
+            appropriateFor: URL(fileURLWithPath: ".", isDirectory: true),
+            create: true
+        )
+        defer {
+            do {
+                try FileManager.default.removeItem(at: temp)
+            } catch {
+                print("Failed to erase temporary directory \(temp.path)", to: &StandardErrorOutputStream.stream)
+            }
+        }
+
+        print("🍒 Generating documentation")
+
+        let files = try createUnprocessed(target: temp)
+        try processAsciiDoc(files: files, to: temp, with: self.processor)
+        let htmlFiles = try FileManager.default.contentsOfDirectory(
+            at: temp.appendingPathComponent("html"),
+            includingPropertiesForKeys: nil,
+            options: [.skipsSubdirectoryDescendants]
+        )
+        let docsFolder = URL(fileURLWithPath: "docs", isDirectory: true)
+        for file in htmlFiles {
+            let outputName = file.lastPathComponent == "Index.html" ? "index.html" : file.lastPathComponent
+            print("🍒 Copying documentation file \(outputName)")
+            try FileManager.default.copyItem(at: file, to: docsFolder.appendingPathComponent(outputName))
+        }
+
+        print("🍒 Committing changes")
+        try runNoisyFromPath("git", args: ["commit", "-m", "Updated documentation"])
+    }
+}
+
+private func errorExit(_ message: String? = nil, _ code: Int32 = 1) -> Error {
+    if let message = message {
+        print(message, to: &StandardErrorOutputStream.stream)
+    }
+    return ExitCode(code)
+}
+
+extension Data {
+    fileprivate var utf8String: String? {
+        String(data: self, encoding: .utf8)
     }
 }
 
@@ -105,4 +192,47 @@ extension TargetPath {
         try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
         self.name = url
     }
+}
+
+struct CommandErrorExit: Error {
+    var code: Int
+}
+
+@discardableResult
+@available(macOS 11.0, *)
+private func runCapturingFromPath(_ command: String, args: [String], cwd: URL? = nil) throws -> Data {
+    let stdout = Pipe()
+    let proc = Process()
+    proc.currentDirectoryURL = cwd
+    proc.executableURL = URL(fileURLWithPath: "/usr/bin/env", isDirectory: false)
+    proc.arguments = [command] + args
+    proc.standardOutput = stdout
+    try proc.run()
+    proc.waitUntilExit()
+    guard proc.terminationStatus == 0 else {
+        throw CommandErrorExit(code: Int(proc.terminationStatus))
+    }
+    let outputData = try stdout.fileHandleForReading.readToEnd()
+    return outputData ?? Data()
+}
+
+@available(macOS 11.0, *)
+private func runNoisyFromPath(_ command: String, args: [String], cwd: URL? = nil) throws {
+    let proc = Process()
+    proc.currentDirectoryURL = cwd
+    proc.executableURL = URL(fileURLWithPath: "/usr/bin/env", isDirectory: false)
+    proc.arguments = [command] + args
+    try proc.run()
+    proc.waitUntilExit()
+    guard proc.terminationStatus == 0 else {
+        throw CommandErrorExit(code: Int(proc.terminationStatus))
+    }
+}
+
+private final class StandardErrorOutputStream: TextOutputStream {
+    func write(_ string: String) {
+        FileHandle.standardError.write(Data(string.utf8))
+    }
+
+    static var stream = StandardErrorOutputStream()
 }
